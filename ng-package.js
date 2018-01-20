fb9de4bac0c988076b39cd13d68afe447bb9d99b
Alan Agius <alan.agius4@gmail.com>
2018-01-20 12:21:05 +0100

    chore(build): add `umdModuleIds` mappings

--- ng-package.js
+++ ng-package.js
@@ -2,8 +2,13 @@ module.exports = {
 	lib: {
 		entryFile: "src/public-api.ts",
 		umdModuleIds: {
+			// vendors
 			"tslib": "tslib",
-			"lodash": "_"
+			"lodash": "_",
+
+			// local
+			"@speedy/package-1": "speedy.package-1",
+			"@speedy/package-1/testing": "speedy.package-1.testing",
 		}
 	}
 }
