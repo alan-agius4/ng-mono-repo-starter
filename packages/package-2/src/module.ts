53a12b9109c3ebc64ea202328e620e7167092345
Alan Agius <alan.agius4@gmail.com>
2018-02-01 11:08:51 +0100

    chore(build): improve typescript module resolution speed

--- packages/package-2/src/module.ts
+++ packages/package-2/src/module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Package1Module } from "@speedy/package-1";
-import { Package1TestingModule } from "@speedy/package-1/testing";
 import { SkeletonService } from "./skeleton/skeleton.service";
 
 @NgModule({
 	imports: [
-		Package1TestingModule,
 		Package1Module
 	],
 	providers: [
