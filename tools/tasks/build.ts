a9b70d683039a88a15c2dc662c23dae9bd1965b4
Alan Agius <alan.agius4@gmail.com>
2018-02-03 12:11:57 +0100

    chore(build): update build task and remove patches

--- tools/tasks/build.ts
+++ tools/tasks/build.ts
@@ -1,7 +1,5 @@
-const ngc = require("ng-packagr/lib/steps/ngc");
-import { build, BuildStep } from "ng-packagr";
+import { ngPackagr } from "ng-packagr";
 import { relative } from "path";
-import { readConfiguration } from "@angular/compiler-cli";
 import { Bundler } from "scss-bundle";
 import { writeFile } from "fs-extra";
 
@@ -30,21 +28,9 @@ async function bundleScss() {
 	}
 }
 
-// this patch is required because of
-// https://github.com/dherges/ng-packagr/issues/473
-const originalPrepareTsConfig = ngc.prepareTsConfig;
-const prepareTsConfigPatched: BuildStep = ({ artefacts, entryPoint, pkg }) => {
-	originalPrepareTsConfig({ artefacts, entryPoint, pkg });
-
-	const { options } = readConfiguration("../../tsconfig.build.json");
-	artefacts.tsConfig.options = {
-		...options,
-		...artefacts.tsConfig.options
-	};
-};
-
-ngc.prepareTsConfig = prepareTsConfigPatched;
-
-build({ project: "ng-package.js" })!
+ngPackagr()
+	.forProject("./ng-package.js")
+	.withTsConfig("../../tsconfig.build.json")
+	.build()
 	.then(bundleScss)
 	.catch(() => process.exitCode = 1);
