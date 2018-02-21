5b81bdd13bf4f568a90015c1076b1e64c3d76499
Alan Agius <alan.agius4@gmail.com>
2018-02-21 11:27:00 +0100

    Update skeleton.component.spec.ts

--- packages/package-1/src/skeleton/skeleton.component.spec.ts
+++ packages/package-1/src/skeleton/skeleton.component.spec.ts
@@ -1,41 +1,27 @@
-import { Component } from "@angular/core";
 import { async, TestBed } from "@angular/core/testing";
 
 import { SkeletonComponent } from "./skeleton.component";
 
-@Component({
-	template: `<obg-skeleton></obg-skeleton>`
-})
-export class TestComponent {
-
-}
-
-beforeEach(() => {
-	TestBed.configureTestingModule({
-		declarations: [
-			TestComponent,
-			SkeletonComponent
-		]
-	});
-});
-
 describe("SkeletonComponentSpecs", () => {
 
 	beforeEach(async(() => {
-		TestBed.compileComponents();
+		TestBed.configureTestingModule({
+			declarations: [
+				SkeletonComponent
+			]
+		}).compileComponents();
 	}));
-
 	describe("given component is initialized", () => {
 		let element: HTMLHeadingElement;
 
 		beforeEach(() => {
-			const fixture = TestBed.createComponent(TestComponent);
+			const fixture = TestBed.createComponent(SkeletonComponent);
 			fixture.detectChanges();
 
 			element = fixture.debugElement.nativeElement.querySelector("h1") as HTMLHeadingElement;
 		});
 
-		test("should have the text set", async(() => {
+		it("should have the text set", async(() => {
 			expect(element.textContent).toMatch("Sample");
 		}));
 	});
