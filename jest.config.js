1dcf88047009db85cd3ecbaf74a5976e86805be0
Alan Agius <alan.agius4@gmail.com>
2018-03-12 10:25:50 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -33,7 +33,8 @@ module.exports = {
 		"<rootDir>/packages/*"
 	],
 	collectCoverageFrom: [
-		"packages/*/src/**/*.ts",
+		"packages/*/*/src/**/*.ts",
+		"packages/*/src/**/*.ts"
 	],
 	coveragePathIgnorePatterns: [
 		".*(spec|const|config|mock|module|public-api|index|mock|model).ts"
