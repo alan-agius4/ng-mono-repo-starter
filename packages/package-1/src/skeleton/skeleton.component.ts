8aa779a8aa82c3b0a475cb62e5837efdc14219f7
Alan Agius <alan.agius4@gmail.com>
2018-03-27 15:31:10 +0200

    Update skeleton.component.ts

--- packages/package-1/src/skeleton/skeleton.component.ts
+++ packages/package-1/src/skeleton/skeleton.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
 	selector: "obg-skeleton",
 	templateUrl: "./skeleton.component.html",
-	styleUrls: ["./skeleton.component.scss"]
+	styleUrls: ["./skeleton.component.scss"],
+	preserveWhitespaces: false
 })
 export class SkeletonComponent {
 
